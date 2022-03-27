df391e289dbfcaaac5183949102d7bd12c50024e
Daniel Franco <82846096+Franccodan@users.noreply.github.com>
2022-03-27 14:10:54 -0300

    Add files via upload

---
+++ Aula 08/script.js
@@ -0,0 +1,64 @@
+/* 
+
+== igualdade valores
+
+=== igualdade a valores e tipos
+
+!== diferença valores e tipos
+
+!= diferença de valores
+
+> maior que
+
+< menor que 
+
+>= maior e igual
+
+<= menor e igual
+
+*/
+
+
+// == igualdade entre valores
+let a = 10
+let b = 10
+console.log(a == b)
+
+//=== igualdade entre valores e tipos 
+let c = 20
+let d = '20'
+console.log(c === d)
+
+
+//diferença valores e tipos !==
+let e = 30
+let f = '30'
+console.log(e !== f)
+
+//diferença entre valores !=
+let g = 40
+let h = '40'
+console.log(g != h)
+
+
+//maior que >
+let i = 20 
+let j = 5
+console.log(i > j)
+
+
+//menor que <
+let k = 40
+let l = 10
+console.log(k < l)
+
+
+//maior e igual >=
+let m = 2
+let n = 9
+console.log(m >= n)
+
+//menor ou igual <=
+let o = 9
+let p = 5
+console.log(o <= p)
